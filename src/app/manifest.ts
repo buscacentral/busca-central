06c3a521b448f06545a7169ab53f3e43e7e2716e
Peixoto <buscacentral@gmail.com>
2026-08-17 15:39:35 -0300

    fix(pwa): atualizar icones PWA e configurar purpose any maskable no manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -17,21 +17,21 @@ export default function manifest(): MetadataRoute.Manifest {
         type: 'image/png',
       },
       {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+      {
         src: '/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'any',
+        purpose: 'any maskable' as any,
       },
       {
         src: '/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any',
-      },
-      {
-        src: '/apple-touch-icon.png',
-        sizes: '180x180',
-        type: 'image/png',
+        purpose: 'any maskable' as any,
       },
     ],
   };
